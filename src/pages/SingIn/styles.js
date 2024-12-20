2cbf2eae56ea1a83d4f7ce279edc4bf386921e10
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2024-12-20 11:49:20 -0300

    Add specific styles for singIn page

--- src/pages/SingIn/styles.js
+++ src/pages/SingIn/styles.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 7.369rem;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`
 
-export const Title = styled.div``
+export const Title = styled.div`
+    display: flex;
+    gap: 1.074rem;
+
+    h1 {
+        font-size: 3.724rem;
+        font-family: 'Roboto', sans-serif;
+        font-weight: bold;
+    }
+
+    svg{
+        width: 4.331rem;
+    }
+`
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
+`
